7e76989863d72e35e022a1e60ef8a62980711b84
Satyush18 <23051376@kiit.ac.in>
2026-04-20 08:50:13 +0530

    Update app_py (1).py

--- app_py (1).py
+++ app_py (1).py
@@ -68,8 +68,11 @@ if uploaded_file:
     sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
 threshold = 0.4
 
-if len(sorted_scores) > 0 and sorted_scores[0][1] >= threshold:
-    best_role = sorted_scores[0][0]
+if sorted_scores:
+    if sorted_scores[0][1] >= threshold:
+        best_role = sorted_scores[0][0]
+    else:
+        best_role = None
 else:
     best_role = None
 
